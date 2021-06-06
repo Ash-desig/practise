ec428723344e191f06ee27806dc6a4165453228f
Ash-desig <gargashi200@gmail.com>
2021-06-06 13:06:52 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from "react";
 import "./style.css";
 
 export default class App extends React.Component {
-  state={
-    name:"ashi"
-  };
+  constructor(props){
+    super(props);
+    this.state={
+      name:"Ashi",
+      roll:this.props.roll
+    };
+  }
+  handleClick=()=>{
+    this.setState({name:"Ashi Garg"})
+  }
+  
   render(){
    return (
       <div>
         <h1>Hello {this.state.name}</h1>
+        <h2> Roll no. {this.state.roll}</h2>
+        <button onClick={this.handleClick}>click me</button>
       </div>
     );
   }
